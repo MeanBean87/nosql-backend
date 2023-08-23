023bf0d48f50c8198ca8d65d81f9a1d158e23331
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-22 22:20:29 -0400

    Created Thoughts Schema.

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const ThoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [ReactionSchema],
+});
+
+ThoughtSchema.virtual("formattedDate").get(function () {
+  const date = new Date(this.createdAt);
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const year = date.getFullYear();
+  return `${month}/${day}/${year}`;
+});
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
+module.exports = Thought;
